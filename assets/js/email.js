d2fe491ba413e9dfa61c8b5f3ffa58f555cfc1df
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-07-23 09:52:30 +0400

    get in touch

--- assets/js/email.js
+++ assets/js/email.js
@@ -4,14 +4,17 @@ function emailSend(){
     var email = document.getElementById('email').value;
     var message = document.getElementById('message').value;
 
+    var messageBody = "Name" + userName +
+    "<br/> Email" + email +
+    "<br/> Message" + message;
     Email.send({
         Host : "smtp.elasticemail.com",
-        Username : "jahezjohn18950@gmail.com",
-        Password : "A1C07BB890884E80EFCA45CA6DCBF6FF9BFC",
+        Username : "22mca018@grd.edu.in",
+        Password : "B9F2C7CA7FB411AE0AD5588CF35A3E775A8D",
         To : 'jahezjohn18950@gmail.com',
         From : "jahezjohn18950@gmail.com",
         Subject : "This is the subject",
-        Body : "And this is the body"
+        Body : messageBody
     }).then(
       message => alert(message)
     );  
